b94cdc3032b026421b37bae717a4e2a5d939909f
Stas-Ukrop <st.kr342@gmail.com>
2022-09-25 22:49:10 +0200

    fix

---
+++ model/cat.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+import mongoosePaginate from 'mongoose-paginate-v2'
+const {Schema, model,SchemaTypes} = mongoose
+
+const catSchema = new Schema({
+    name: String,
+    age: {
+        type: Number,
+        min: 0,
+        max: 35
+    },
+    isVaccinated: {
+        type: Boolean,
+        default: false
+    },
+    owner: {
+        type:mongoose.SchemaTypes.ObjectId,ref:'user'
+    },
+    features: {
+        type: Array,
+        set: (data) => (!data ? [] : data)
+    }
+}, {
+    versionKey: false,
+    timestamps: true
+},)
+catSchema.plugin(mongoosePaginate)
+const Cat = model('cat', catSchema)
+
+export default Cat
